faa71fdb91a10696c09641eb9b9ad5457424fa14
ZP4 <jasonhuzheming9@gmail.com>
2020-10-23 23:02:22 -0400

    Project init

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+const db = require("./config/keys.js").mongoURI;
+
+mongoose
+    .connect(db, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then( () => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}` ));
+    
