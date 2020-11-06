c6278bb056d68034fedc7dc65a7342603b6c037f
ZP4 <jasonhuzheming9@gmail.com>
2020-11-06 00:21:01 -0500

    Redirect if user is not logged in or is logged in (foolproof design)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,57 @@
-import React from 'react';
-import HomePage from './Homepage/HomePage';
-import MoodPage from './MoodPage/MoodPage';
-import PlaylistPage from './components/Playlists'
-import LandingPage from './LandingPage/LandingPage';
-import BrowsePage from './components/Browse';
-import SettingsPage from './components/Settings'
-import AdminPage from './components/Admin'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import React, { useState } from "react";
+import HomePage from "./Homepage/HomePage";
+import MoodPage from "./MoodPage/MoodPage";
+import PlaylistPage from "./components/Playlists";
+import LandingPage from "./LandingPage/LandingPage";
+import BrowsePage from "./components/Browse";
+import SettingsPage from "./components/Settings";
+import AdminPage from "./components/Admin";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
 function App() {
+  const [auth, setAuth] = useState(false);
+
   return (
     <Router>
       <Switch>
-        <Route path='/' exact={true} component={LandingPage}/>
-        <Route path = '/home' exact = {true} component = {HomePage} />
-        <Route path = '/mood' exact = {true} component = {MoodPage}/>
-        <Route path = '/land' exact = {true} component = {LandingPage} />
-        <Route path= '/settings' exact = {true} component = {SettingsPage} />
-        <Route path = '/admin' exact = {true} component = {AdminPage} />
+        <Route
+          path="/"
+          exact={true}
+          render={() => (auth ? <Redirect to="/home" /> : <LandingPage />)}
+        />
+        <Route
+          path="/home"
+          exact={true}
+          render={() => (!auth ? <Redirect to="/" /> : <HomePage />)}
+        />
+        <Route
+          path="/mood"
+          exact={true}
+          render={() => (!auth ? <Redirect to="/home" /> : <MoodPage />)}
+        />
+        <Route
+          path="/settings"
+          exact={true}
+          render={() => (!auth ? <Redirect to="/" /> : <SettingsPage />)}
+        />
+        <Route
+          path="/admin"
+          exact={true}
+          component={AdminPage}
+          onEnter={auth}
+          render={() => (!auth ? <Redirect to="/" /> : <SettingsPage />)}
+        />
       </Switch>
     </Router>
   );
 }
 
-
 export default App;
-
