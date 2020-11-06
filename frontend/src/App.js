4ebd86c2decfbd8373575f8005e82815d07fb05a
ZP4 <jasonhuzheming9@gmail.com>
2020-11-06 13:45:11 -0500

    Added cookiehandler.js and session for logins

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useContext, createContext, useEffect } from "react";
 import HomePage from "./Homepage/HomePage";
 import MoodPage from "./MoodPage/MoodPage";
 import PlaylistPage from "./components/Playlists";
 import LandingPage from "./LandingPage/LandingPage";
-import BrowsePage from "./components/Browse";
 import SettingsPage from "./components/Settings";
 import AdminPage from "./components/Admin";
+import { createBrowserHistory } from "history";
+import { getSessionCookie, setSessionCookie } from "./CookieHandler";
 import {
   BrowserRouter as Router,
   Switch,
@@ -16,39 +17,78 @@ import {
 
 import "./App.css";
 
+export const AppContext = createContext();
+const history = createBrowserHistory();
+
 function App() {
   const [auth, setAuth] = useState(false);
+  const [session, setSession] = useState(getSessionCookie());
+
+  useEffect(() => {
+    setSession(getSessionCookie());
+  }, []);
 
   return (
-    <Router>
-      <Switch>
-        <Route
-          path="/"
-          exact={true}
-          render={() => (auth ? <Redirect to="/home" /> : <LandingPage />)}
-        />
-        <Route
-          path="/home"
-          exact={true}
-          render={() => (!auth ? <Redirect to="/" /> : <HomePage />)}
-        />
-        <Route
-          path="/mood"
-          exact={true}
-          render={() => (!auth ? <Redirect to="/home" /> : <MoodPage />)}
-        />
-        <Route
-          path="/settings"
-          exact={true}
-          render={() => (!auth ? <Redirect to="/" /> : <SettingsPage />)}
-        />
-        <Route
-          path="/admin"
-          exact={true}
-          render={() => (!auth ? <Redirect to="/" /> : <AdminPage />)}
-        />
-      </Switch>
-    </Router>
+    <AppContext.Provider value={{ session }}>
+      <Router history={history}>
+        <Switch>
+          <Route
+            path="/land"
+            exact={true}
+            render={() =>
+              !session.username === undefined ? (
+                <Redirect to="/" />
+              ) : (
+                <LandingPage />
+              )
+            }
+          />
+          <Route
+            path="/mood"
+            exact={true}
+            render={() =>
+              session.username === undefined ? (
+                <Redirect to="/land" />
+              ) : (
+                <MoodPage />
+              )
+            }
+          />
+          <Route
+            path="/settings"
+            exact={true}
+            render={() =>
+              session.username === undefined ? (
+                <Redirect to="/land" />
+              ) : (
+                <SettingsPage />
+              )
+            }
+          />
+          <Route
+            path="/admin"
+            exact={true}
+            render={() =>
+              session.username === undefined ? (
+                <Redirect to="/land" />
+              ) : (
+                <AdminPage />
+              )
+            }
+          />
+          <Route
+            path="/"
+            render={() =>
+              session.username === undefined ? (
+                <Redirect to="/land" />
+              ) : (
+                <HomePage />
+              )
+            }
+          />
+        </Switch>
+      </Router>
+    </AppContext.Provider>
   );
 }
 
--- frontend/src/components/Admin.js
+++ frontend/src/components/Admin.js
@@ -1,162 +1,137 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
-import './Admin.css';
-import SearchBar from 'material-ui-search-bar';
-import userdata from '../data/admintest.json';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Admin.css";
+import SearchBar from "material-ui-search-bar";
+import userdata from "../data/admintest.json";
 
-import styled from 'styled-components'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-pro-sidebar/dist/css/styles.css';
-import { ButtonBase } from '@material-ui/core';
+import styled from "styled-components";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-pro-sidebar/dist/css/styles.css";
+import { ButtonBase } from "@material-ui/core";
 
 const Button = styled.button`
-    padding: .5em;
-    color: Black;
-    border-radius: 10px;
-    margin: .5em;
-    font-size: 30px;
-    font-family: 'Roboto', sans-serif;
-    background-color: light-grey;
-`
+  padding: 0.5em;
+  color: Black;
+  border-radius: 10px;
+  margin: 0.5em;
+  font-size: 30px;
+  font-family: "Roboto", sans-serif;
+  background-color: light-grey;
+`;
 
 const CButton = styled.button`
-    padding: .5em;
-    color: Black;
-    border-radius: 10px;
-    margin: .5em;
-    font-size: 15px;
-    font-family: 'Roboto', sans-serif;
-    background-color: light-grey;
-`
+  padding: 0.5em;
+  color: Black;
+  border-radius: 10px;
+  margin: 0.5em;
+  font-size: 15px;
+  font-family: "Roboto", sans-serif;
+  background-color: light-grey;
+`;
 
 const CenterDiv = styled.div`
-    height: 100vh;
-    width: 100vw;
-    background:linear-gradient(rgb(46, 0, 48),transparent);
-    background-color:rgb(77, 77, 75);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-`
+  height: 100vh;
+  width: 100vw;
+  background: linear-gradient(rgb(46, 0, 48), transparent);
+  background-color: rgb(77, 77, 75);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
 const CenterPanel = styled.div`
-    color: white;
-    display: flex;
-    height: 50vh;
-    justify-content: center;
-    background:linear-gradient(rgb(46, 0, 48),transparent);
-    background-color:rgb(77, 77, 75);
-
-`
+  color: white;
+  display: flex;
+  height: 50vh;
+  justify-content: center;
+  background: linear-gradient(rgb(46, 0, 48), transparent);
+  background-color: rgb(77, 77, 75);
+`;
 const ResultsPanel = styled.div`
-    color: white;
-    margin-top: 3%;
-    margin-left: 3%;
-    position: absolute;
-    margin-right: 20%;
-    font-size: 24px;
-`
+  color: white;
+  margin-top: 3%;
+  margin-left: 3%;
+  position: absolute;
+  margin-right: 20%;
+  font-size: 24px;
+`;
 
 const StyledSearch = styled(SearchBar)`
-    max-height: 2em;
-    margin-left: 1em;
-    margin-right: 1em;
-    
-`
-function Admin()
-{
-    const [currF, setcurrF] = useState("");
+  max-height: 2em;
+  margin-left: 1em;
+  margin-right: 1em;
+`;
+function Admin() {
+  const [currF, setcurrF] = useState("");
 
-    return(
-        <CenterDiv className = "fullscreen-container">
-            <div id = "atxt">
-                Admin
-            </div>
+  return (
+    <CenterDiv className="fullscreen-container">
+      <div id="atxt">Admin</div>
 
-            <div>
-                <Button
-                onClick = {() => setcurrF("Ban User")}
-                >Ban User</Button>
-                <Button
-                onClick = {() => setcurrF("Unban User")}
-                >Unban User</Button>
-                <Button
-                onClick = {() => setcurrF("Remove User")}
-                >Remove User</Button>
-                
-                {ScreenType(currF)}
+      <div>
+        <Button onClick={() => setcurrF("Ban User")}>Ban User</Button>
+        <Button onClick={() => setcurrF("Unban User")}>Unban User</Button>
+        <Button onClick={() => setcurrF("Remove User")}>Remove User</Button>
 
-            </div>
+        {ScreenType(currF)}
+      </div>
+    </CenterDiv>
+  );
+}
+function ScreenType(focus) {
+  const [currS, setcurrS] = useState("");
 
-            
-        </CenterDiv>
+  if (focus === "") return <div></div>;
+  else {
+    return (
+      <CenterPanel>
+        <StyledSearch onChange={(val) => setcurrS(val)} />
+        <ResultsPanel>
+          {
+            //go through database and print out people (excluding admin)
+            userdata.users.map((user) => {
+              if (
+                user.username.includes(currS) &&
+                user.username !== "Administrator"
+              ) {
+                return (
+                  <div>
+                    {user.username}
+                    <CButton
+                      onClick={() => RB(user.account_type, focus, user._id)}
+                    >
+                      {focus}
+                    </CButton>
+                  </div>
+                );
+              }
+            })
+          }
+        </ResultsPanel>
+      </CenterPanel>
     );
-}
-function ScreenType(focus)
-{
-    const [currS, setcurrS] = useState("");
-
-    if (focus === "")
-        return <div></div>
-    else
-    {
-        return(
-            <CenterPanel>
-                <StyledSearch 
-                onChange = {(val) => setcurrS(val)}
-                />
-                <ResultsPanel>
-                    { //go through database and print out people (excluding admin)
-                    userdata.users.map( (user) =>
-                    {
-                        if((user.username).includes(currS) && user.username !== "Administrator")
-                        {
-                            return(
-                                <div>
-                                    {user.username}
-                                    <CButton
-                                    onClick = {() => RB(user.account_type,focus,user._id)}
-                                    >{focus}</CButton>
-                                </div>
-
-                            );
-                        }
-                    })
-                    }
-                </ResultsPanel>
-            </CenterPanel>
-    
-        );
-    }
+  }
 }
 
-function RB(acc_type,focus,id) //id used to find the user
-{
-    if(acc_type !== 1 && focus === "Ban User") //When admin tried to ban an already banned user
-    {
-        alert("Cannot ban! User is already banned.");
-    }
-    else if (acc_type === 1 && focus === "Ban User") //Admin bans user
-    {
-        alert("User has been banned.");
-        //backend functionality here
-    }
-    else if(acc_type !== -1 && focus === "Unban User") //Admin tried unbanning a user that was not banned
-    {
-        alert("Cannot unban user. User is already unbanned.")
-    }
-    else if(acc_type === -1 && focus === "Unban User") // Admin unbans user
-    {
-        alert("User has been unbanned.")
-        //backend functionality here
-    }
-    else if(focus === "Remove User" )
-    {
-        alert("User removed.")
-        //backend functionality here
-    }
+function RB(acc_type, focus, id) {
+  //id used to find the user
+  if (acc_type !== 1 && focus === "Ban User") {
+    //When admin tried to ban an already banned user
+    alert("Cannot ban! User is already banned.");
+  } else if (acc_type === 1 && focus === "Ban User") {
+    //Admin bans user
+    alert("User has been banned.");
+    //backend functionality here
+  } else if (acc_type !== -1 && focus === "Unban User") {
+    //Admin tried unbanning a user that was not banned
+    alert("Cannot unban user. User is already unbanned.");
+  } else if (acc_type === -1 && focus === "Unban User") {
+    // Admin unbans user
+    alert("User has been unbanned.");
+    //backend functionality here
+  } else if (focus === "Remove User") {
+    alert("User removed.");
+    //backend functionality here
+  }
 }
-
-
 
 export default Admin;
